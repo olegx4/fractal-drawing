0a2833080052e5a293b21507d2fdc0de24d08b46
Oleh <olegx4@ukr.net>
2020-07-16 20:04:57 +0300

    Added class of defined figures.

---
+++ src/DefinedFigure.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.util.List;
+
+public class DefinedFigure {
+
+    final List<Point> triangle = List.of(
+            new Point(600, 100),
+            new Point(300, 700),
+            new Point(900, 700),
+            new Point(600, 500));
+
+    final List<Point> square = List.of(
+            new Point(100, 100),
+            new Point(600, 100),
+            new Point(100, 600),
+            new Point(350, 350),
+            new Point(600, 600));
+
+    final List<Point> pentagon = List.of(
+            new Point(100, 400),
+            new Point(600, 100),
+            new Point(1100, 400),
+            new Point(300, 800),
+            new Point(600, 500),
+            new Point(900, 800));
+
+    final List<Point> hexagon = List.of(
+            new Point(0, 200),
+            new Point(600, 0),
+            new Point(1200, 200),
+            new Point(0, 700),
+            new Point(1200, 700),
+            new Point(600, 900));
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        final Point firstPoint = new Point(111, 500);
+        final Point firstPoint = new Point(600, 500);
+        final DefinedFigure definedFigure = new DefinedFigure();
 
-        List<Point> figureTops = new ArrayList<>();
-        figureTops.add(new Point(600, 100));
-        figureTops.add(new Point(100, 400));
-//        figureTops.add(new Point(1900, 400));
-        figureTops.add(new Point(300, 700));
-//        figureTops.add(new Point(900, 700));
-        //figureTops.add(new Point(400, 0));
-//        figureTops.add(new Point(800, 0));
-//        figureTops.add(new Point(200, 200));
-//        figureTops.add(new Point(600, 200));
-//        figureTops.add(new Point(400, 400));
-//        figureTops.add(new Point(800, 400));
-//        figureTops.add(new Point(200, 600));
-//        figureTops.add(new Point(600, 600));
-
-
+        final List<Point> figureTops = definedFigure.triangle;
         Figure figure = new Figure().setFigureTopPoints(figureTops);
 
         final FractalDrawingPanel panel = new FractalDrawingPanel(figure, firstPoint);
